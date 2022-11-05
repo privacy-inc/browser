f127cca3f8dc1828117def9e6e7482c4b7de7312
vauxhall <zero.griffin@gmail.com>
2022-11-05 09:28:46 +0100

    Update empty screen

--- iOS/Detail.swift
+++ iOS/Detail.swift
@@ -8,6 +8,18 @@ struct Detail: View {
         ZStack {
             if let webview = session[tab: id] {
                 Browser(webview: webview)
+            } else {
+                Button {
+                    session.field.becomeFirstResponder()
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .font(.system(size: 32, weight: .semibold))
+                        .foregroundStyle(.tertiary)
+                        .foregroundColor(.secondary)
+                        .contentShape(Rectangle())
+                        .frame(width: 120, height: 120)
+                }
+                .ignoresSafeArea(.keyboard)
             }
             Search(session: session)
                 .frame(height: 0)
