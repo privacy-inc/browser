1c31be3388b2a5870adf64de47d8421fc05a2be4
vauxhall <zero.griffin@gmail.com>
2022-11-21 16:04:37 +0100

    Update bookmark

--- iOS/Bar.Detail.swift
+++ iOS/Bar.Detail.swift
@@ -1,17 +1,21 @@
 import SwiftUI
+import Engine
 
 extension Bar {
     struct Detail: View {
         @ObservedObject var session: Session
         @State private var loading = false
         @State private var reader = false
+        @State private var isBookmark = false
+        @State private var isReadingList = false
         @Environment(\.dismiss) private var dismiss
         
         var body: some View {
             NavigationStack {
                 if case let .tab(id) = session.content,
                    let webview = session[tab: id]?.webview,
-                   webview.url != nil {
+                   let url = webview.url {
+                    
                     Web(session: session, webview: webview)
                         .toolbar {
                             ToolbarItemGroup(placement: .bottomBar) {
@@ -30,23 +34,44 @@ extension Bar {
                                 }
                                 
                                 Button {
-                                    
+                                    guard !isReadingList else { return }
+                                    isReadingList = true
+                                    Task {
+                                        await session.cloud.add(read: .init(url: url.absoluteString,
+                                                                            title: webview.title ?? ""))
+                                    }
                                 } label: {
-                                    Label("Add to reading list", systemImage: "eyeglasses")
+                                    Label("Add to reading list",
+                                          systemImage: isReadingList ? "checkmark.circle.fill" : "eyeglasses")
                                         .symbolRenderingMode(.hierarchical)
                                         .font(.system(size: 15, weight: .bold))
                                         .contentShape(Rectangle())
                                         .frame(minWidth: 50, minHeight: 40)
                                 }
+                                .disabled(isReadingList)
                                 
                                 Button {
-                                    
+                                    Task {
+                                        if isBookmark {
+                                            await session.cloud.delete(bookmark: url.absoluteString)
+                                        } else {
+                                            guard
+                                                let bookmark = Bookmark(url: url.absoluteString,
+                                                                        title: webview.title ?? "")
+                                            else { return }
+                                            await session.cloud.add(bookmark: bookmark)
+                                        }
+                                    }
                                 } label: {
-                                    Label("Add to bookmarks", systemImage: "bookmark")
+                                    Label(isBookmark ? "Remove from bookmarks" : "Add to bookmarks",
+                                          systemImage: isBookmark ? "bookmark.fill" : "bookmark")
                                         .symbolRenderingMode(.hierarchical)
                                         .font(.system(size: 15, weight: .bold))
                                         .contentShape(Rectangle())
                                         .frame(minWidth: 50, minHeight: 40)
+                                }
+                                .onReceive(session.cloud) {
+                                    isBookmark = $0.bookmarks.contains { $0.url == url.absoluteString }
                                 }
                                 
                                 Button {
