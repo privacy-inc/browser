7f40a4a6115a49a9774f0e0f2be658f53ae53218
vauxhall <zero.griffin@gmail.com>
2022-11-25 12:25:08 +0100

    Update downloads

--- iOS/Downloads.swift
+++ iOS/Downloads.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct Downloads: View {
     @ObservedObject var session: Session
+    @State private var alert = false
     
     var body: some View {
         List {
@@ -37,5 +38,28 @@ struct Downloads: View {
         .listStyle(.plain)
         .navigationTitle(Category.downloads.title)
         .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    alert = true
+                } label: {
+                    Image(systemName: "trash.circle.fill")
+                        .foregroundStyle(session.downloads.isEmpty ? .tertiary : .secondary)
+                        .foregroundColor(session.downloads.isEmpty ? .secondary : .pink)
+                        .symbolRenderingMode(.hierarchical)
+                        .font(.system(size: 24, weight: .regular))
+                        .frame(width: 40, height: 36)
+                        .contentShape(Rectangle())
+                }
+                .disabled(session.downloads.isEmpty)
+                .confirmationDialog("Remove all", isPresented: $alert) {
+                    Button("Remove", role: .destructive) {
+                        session.downloads.forEach { $0.item.cancel() }
+                        session.downloads = []
+                    }
+                    Button("Cancel", role: .cancel) { }
+                }
+            }
+        }
     }
 }
