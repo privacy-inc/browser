eaf29b8c236b4c8ed73a7572fe5ea7067269956e
Vicente Garcia <zero.griffin@gmail.com>
2022-12-01 16:22:45 +0100

    Update tabs

--- iOS/Sidebar.swift
+++ iOS/Sidebar.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct Sidebar: View {
     @ObservedObject var session: Session
+    @State private var alert = false
     
     var body: some View {
         List(selection: $session.sidebar) {
@@ -44,16 +45,53 @@ struct Sidebar: View {
             VStack(spacing: 14) {
                 Divider()
                 
-                Button {
-                    session.newTab()
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                        .symbolRenderingMode(.hierarchical)
-                        .font(.system(size: 30, weight: .regular))
-                        .contentShape(Rectangle())
-                        .frame(width: 50, height: 38)
+                ZStack {
+                    HStack {
+                        Button {
+                            alert = true
+                        } label: {
+                            Image(systemName: "trash.circle.fill")
+                                .foregroundStyle(session.tabs.isEmpty ? .tertiary : .secondary)
+                                .foregroundColor(session.tabs.isEmpty ? .secondary : .pink)
+                                .symbolRenderingMode(.hierarchical)
+                                .font(.system(size: 24, weight: .regular))
+                                .contentShape(Rectangle())
+                                .frame(width: 50, height: 38)
+                        }
+                        .padding(.bottom, 8)
+                        .padding(.leading)
+                        .disabled(session.tabs.isEmpty)
+                        .confirmationDialog("Close tabs", isPresented: $alert) {
+                            Button("Close new tabs") {
+                                session.sidebar = nil
+                                session.tabs.removeAll {
+                                    $0.webview == nil
+                                }
+                            }
+                            Button("Close all", role: .destructive) {
+                                session.sidebar = nil
+                                session.tabs.forEach {
+                                    $0.webview?.clean()
+                                }
+                                session.tabs = []
+                            }
+                            Button("Cancel", role: .cancel) { }
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    Button {
+                        session.newTab()
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .symbolRenderingMode(.hierarchical)
+                            .font(.system(size: 30, weight: .regular))
+                            .contentShape(Rectangle())
+                            .frame(width: 50, height: 38)
+                    }
+                    .padding(.bottom, 8)
                 }
-                .padding(.bottom, 8)
             }
             .frame(maxWidth: .greatestFiniteMagnitude)
             .background(Color(.systemBackground))
--- iOS/Tabs.swift
+++ iOS/Tabs.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 struct Tabs: View {
     @ObservedObject var session: Session
-    @State private var alert = false
     
     var body: some View {
         ForEach(session.tabs) { tab in
@@ -22,42 +21,5 @@ struct Tabs: View {
         .onMove { index, destination in
             session.tabs.move(fromOffsets: index, toOffset: destination)
         }
-        
-//        .toolbar {
-//            ToolbarItemGroup(placement: .primaryAction) {
-//                EditButton()
-//                    .disabled(session.tabs.count < 2)
-//
-//                Button {
-//                    alert = true
-//                } label: {
-//                    Image(systemName: "trash.circle.fill")
-//                        .foregroundStyle(session.tabs.isEmpty ? .tertiary : .secondary)
-//                        .foregroundColor(session.tabs.isEmpty ? .secondary : .pink)
-//                        .symbolRenderingMode(.hierarchical)
-//                        .font(.system(size: 24, weight: .regular))
-//                        .frame(width: 40, height: 36)
-//                        .contentShape(Rectangle())
-//                }
-//                .disabled(session.tabs.isEmpty)
-//                .confirmationDialog("Close tabs", isPresented: $alert) {
-//                    Button("Close new tabs") {
-//                        session.tabs.removeAll {
-//                            $0.webview == nil
-//                        }
-//                    }
-//                    Button("Close all", role: .destructive) {
-//                        session.path = []
-//                        session.tabs.forEach {
-//                            $0.webview?.clean()
-//                        }
-//                        session.tabs = []
-//                    }
-//                    Button("Cancel", role: .cancel) { }
-//                }
-//            }
-//            
-            
-//        }
     }
 }
